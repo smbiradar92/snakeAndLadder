f197ff12c9430573d94e447ed579404cf74c3697
Smbiradar92 <smbiradar92@gmail.com>
2022-06-11 10:52:03 +0530

    start the game at position 0

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -1,7 +1,14 @@
 <?php
 
-function welcomeMsg(){
-    echo "Welcome to snake and ladder program";
-} 
-welcomeMsg();
-?>
+class SnakeAndLadder
+{
+    public $position = 0;
+
+    function welcomeMsg()
+    {
+        echo "Welcome to snake and ladder game";
+    }
+}
+
+$play = new SnakeAndLadder();
+$play->welcomeMsg();
