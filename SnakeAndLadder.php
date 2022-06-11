cca30a5c0a5f8a091141f53904a31c69539bc751
Smbiradar92 <smbiradar92@gmail.com>
2022-06-11 20:50:41 +0530

    program to check no play, snake bit and ladder condition

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -3,19 +3,44 @@
 class SnakeAndLadder
 {
     public $position = 0;
+    public $diceNum = 0;
 
     function welcomeMsg()
     {
         echo "Welcome to snake and ladder game \n";
     }
 
-    function diceRoll()
+    // function diceRoll()
+    // {
+    //     $diceNum = rand(1, 6);
+    //     echo "The dice number is : " . $diceNum . "\n";
+    // }
+
+    function gamePlay()
     {
         $diceNum = rand(1, 6);
         echo "The dice number is : " . $diceNum . "\n";
+        $options = rand(0, 2);
+        switch ($options) {
+            case 0:
+                echo "You have got a no play!!\nYou stay in the same position";
+                break;
+            case 1:
+                echo "You are bit by a snake!!\n";
+                $this->poistion = $this->position - $diceNum;
+                if ($this->position < 0) {
+                    $diceNum = 0;
+                    $this->poistion = $this->position - $diceNum;
+                }
+                echo "your current position is " . $this->position;
+                break;
+            case 2:
+                echo "You have recahed a ladder:)\nYou can move ahead by $diceNum numbers.";
+                $this->poistion = $this->position + $diceNum;
+                break;
+        }
     }
 }
-
 $play = new SnakeAndLadder();
 $play->welcomeMsg();
-$play->diceRoll();
+$play->gamePlay();
