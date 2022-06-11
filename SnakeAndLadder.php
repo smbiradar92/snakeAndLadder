c8913516e3f30c66498939aeff42e3b2e008753d
Smbiradar92 <smbiradar92@gmail.com>
2022-06-11 23:06:30 +0530

    adding program for noPlay, snakeBite and ladder condition

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -18,29 +18,31 @@ class SnakeAndLadder
 
     function gamePlay()
     {
-        $diceNum = rand(1, 6);
-        echo "The dice number is : " . $diceNum . "\n";
+        $this->diceNum = rand(1, 6);
+        echo "The dice number is : " . $this->diceNum . "\n";
         $options = rand(0, 2);
         switch ($options) {
             case 0:
-                echo "You have got a no play!!\nYou stay in the same position";
+                echo "You have got a no play!!\nYour current position is " . $this->position . "\n";
+                $this->position = $this->position;
                 break;
             case 1:
-                echo "You are bit by a snake!!\n";
-                $this->poistion = $this->position - $diceNum;
+                echo "You are bit by a snake!!\n";                                  //if player position at snake bit then the position will be added 
+                $this->position = $this->position - $this->diceNum;                 //with dice number to avoid negative value
                 if ($this->position < 0) {
-                    $diceNum = 0;
-                    $this->poistion = $this->position - $diceNum;
+                    $this->position = $this->position + $this->diceNum;
                 }
-                echo "your current position is " . $this->position;
+                echo "your current position is " . $this->position . "\n";
                 break;
             case 2:
-                echo "You have recahed a ladder:)\nYou can move ahead by $diceNum numbers.";
-                $this->poistion = $this->position + $diceNum;
+                echo "You have recahed a ladder:)\nYou can move ahead by $this->diceNum numbers.\n";
+                $this->position = $this->position + $this->diceNum;                                     // at ladder position the player moves by dice number value 
+                echo "your current position is " . $this->position . "\n";
                 break;
         }
     }
 }
+
 $play = new SnakeAndLadder();
 $play->welcomeMsg();
 $play->gamePlay();
