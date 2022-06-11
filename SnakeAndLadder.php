bc8f5249b2c4ffeb2a7e0aa2b2a209d0b1d2920a
Smbiradar92 <smbiradar92@gmail.com>
2022-06-11 11:13:40 +0530

    program to roll  dice to get a random number

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -6,9 +6,16 @@ class SnakeAndLadder
 
     function welcomeMsg()
     {
-        echo "Welcome to snake and ladder game";
+        echo "Welcome to snake and ladder game \n";
+    }
+
+    function diceRoll()
+    {
+        $diceNum = rand(1, 6);
+        echo "The dice number is : " . $diceNum . "\n";
     }
 }
 
 $play = new SnakeAndLadder();
 $play->welcomeMsg();
+$play->diceRoll();
